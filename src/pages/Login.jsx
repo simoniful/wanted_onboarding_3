2e2b59a934420eaa1a88a8b3df9784097584ccb1
Myoung Jin Jang <thinkjin6@gmail.com>
2021-08-03 22:59:58 +0900

    Design: 로그인/회원가입 페이지 스타일링

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+import { SignIn, SignUp } from 'components';
 
 const Login = () => (
-  <>
-    <h1>Login Page</h1>
-  </>
+  <Container>
+    <h2>Jaranda</h2>
+    <SignIn />
+    <SignUp />
+  </Container>
 );
 
 export default Login;
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+
+  h2 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 100;
+    transform: translate(15%, 50%);
+    color: #fff;
+    font-size: 30px;
+  }
+`;
