36c1e79653bf8eab3293c7b3aee060e980ed04c0
sanghunlee <simoniful0gmail.com>
2021-08-05 00:58:50 +0900

    Refactor: #32 validation 문구 수정

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -12,7 +12,7 @@ export const regValidate = (value, regExp) => {
 export function validate(values) {
   const errors = {};
   if (!values.id) {
-    errors.id = '아이디를 입력하세요';
+    errors.id = '아이디를 입력하세요.';
   } else if (!regValidate(values.id, REGEXP.id)) {
     errors.id = '아이디는 4~12자의 영문 대소문자와 숫자로만 입력하여 주세요.';
   }
@@ -20,22 +20,24 @@ export function validate(values) {
     errors.password = '비밀번호를 입력하세요';
   } else if (!regValidate(values.password, REGEXP.password)) {
     errors.password = '비밀번호는 4~16자의 영문 대소문자와 숫자로만 입력하여 주세요.';
+  } else if (values.password === values.id) {
+    errors.password = '비밀번호는 아이디와 동일할 수 없습니다.';
   }
 
   if (!values.checkingPassword) {
-    errors.checkingPassword = '비밀번호를 다시 입력하세요';
+    errors.checkingPassword = '비밀번호를 다시 입력하세요.';
   } else if (values.checkingPassword !== values.password) {
     errors.checkingPassword = '비밀번호가 일치하지 않습니다.';
   }
 
   if (!values.name) {
-    errors.name = '이름을 입력하세요';
+    errors.name = '이름을 입력하세요.';
   } else if (!regValidate(values.name, REGEXP.name)) {
-    errors.name = '이름은 한글 2~4자로 입력하여 주세요.';
+    errors.name = '특수문자,영어,숫자는 사용할수 없습니다. 한글 2~4자 이내로 입력해주세요.';
   }
 
   if (!values.age) {
-    errors.age = '나이를 입력하세요';
+    errors.age = '나이를 입력하세요.';
   } else if (!Number.isInteger(+values.age)) {
     errors.age = '나이는 숫자로 입력해주세요.';
   }
