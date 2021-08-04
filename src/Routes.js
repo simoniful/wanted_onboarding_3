b26c72b856ea0454a564f2058eca0533b9711429
sanghunlee <simoniful0gmail.com>
2021-08-04 11:07:17 +0900

    Refactor: routes 경로 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Signup from 'pages/Signup';
+import Signup from './pages/Signup';
 import Login from './pages/Login';
 import User from './pages/User';
 import Admin from './pages/Admin';
