41d82d90d61a9131d52ceb27b6fbe3e36249c4fa
Taekhun <nth9708@naver.com>
2021-08-06 01:24:22 +0900

    Feat: Admin/User 권한 관련 PrivateRoute 구현
    Ref: #47

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { checkAuth } from 'utils/auth';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import User from './pages/User';
 import Admin from './pages/Admin';
@@ -8,10 +8,23 @@ const Routes = () => (
   <Router>
     <Switch>
       <Route exact path='/' component={Login} />
-      <Route exact path='/user' component={User} />
-      <Route exact path='/admin' component={Admin} />
+      <PrivateRoute
+        exact
+        path='/user'
+        userTypes={['teacher', 'parent']}
+        component={User}></PrivateRoute>
+      <PrivateRoute exact path='/admin' userTypes={['admin']} component={Admin}></PrivateRoute>
     </Switch>
   </Router>
 );
+
+const PrivateRoute = ({ component: Component, userTypes: _userTypes, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => (checkAuth(_userTypes) ? <Component {...props} /> : <Redirect to='/' />)}
+    />
+  );
+};
 
 export default Routes;
