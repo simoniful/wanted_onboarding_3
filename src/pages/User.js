4e0f00430002e77c20ae41686a2a5c1759b6bd52
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-08-06 16:22:27 +0900

    feat: USer 페이지 구조 수정 Relves #65

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,17 +1,14 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
 import { layouts as S } from 'styles/layouts';
 import GlobalStyles from 'styles/GlobalStyles';
 import Navbar from 'components/Navbar';
 import { AccountButton } from 'components';
-
-import styled from 'styled-components';
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 import { getLocalStorage } from 'utils/storage';
 import { LOGIN_USER } from '../utils/config';
 import { logout } from '../utils/auth';
-// import Graph from 'components/Chart';
-// import UserTable from 'components/UserTable';
-// import SearchBox from 'components/SearchBox';
+import { COLOR_STYLES } from '../styles/styles';
 
 const User = () => {
   const history = useHistory();
@@ -30,36 +27,92 @@ const User = () => {
       <GlobalStyles />
       <S.Wrap>
         <Navbar name={loginUser.name} />
-        <S.Container>
-          <S.Section>
+        <UserContainer>
+          <UserSection>
             <S.Content>
-              <h1>{`어서오세요 ${
-                loginUser.userType === `teacher` ? `선생님` : `부모님`
-              } 페이지입니다`}</h1>
-            </S.Content>
-            <S.Aside>
-              <AccountButton onClick={onLogout} content='로그아웃' />
+              <PageText>
+                {`어서오세요 자란다 ${
+                  loginUser.userType === `teacher` ? `선생님` : `부모님`
+                } 페이지입니다`}
+              </PageText>
               <MenuList>
                 {menuList.map((sideMenu, key) => (
                   <Menu key={key}>{sideMenu}</Menu>
                 ))}
               </MenuList>
-            </S.Aside>
-          </S.Section>
-        </S.Container>
+              <ContentText>96% 부모님이 추천합니다.</ContentText>
+              <ContentText>딱 맞는 선생님을 무료로 추천 받으세요.</ContentText>
+              <MainImg />
+            </S.Content>
+            <CardAside>
+              <S.Sidebar>
+                <UserAccountBox>
+                  <AccountButton onClick={onLogout} content='로그아웃' />
+                </UserAccountBox>
+              </S.Sidebar>
+            </CardAside>
+          </UserSection>
+        </UserContainer>
       </S.Wrap>
     </>
   );
 };
 
-const MenuList = styled.li`
+const MenuList = styled.ul`
+  display: flex;
   width: 100%;
-  background-color: #bbbbbb;
+  padding: 10px 20px;
+  margin: 50px 0;
+  justify-content: center;
+  color: ${COLOR_STYLES.white};
+  background: ${COLOR_STYLES.primaryGradient};
+  border-radius: 20px;
 `;
 
-const Menu = styled.ul`
+const Menu = styled.li`
   width: 100%;
-  padding: 16px 0;
+  padding: 10px 0;
+  text-align: center;
+`;
+
+const PageText = styled.div`
+  font-size: 20px;
+  margin: 30px 0;
+  text-align: center;
+`;
+
+const ContentText = styled.div`
+  font-size: 20px;
+  margin: 10px 0;
+`;
+
+const CardAside = styled(S.Aside)`
+  width: 40%;
+  min-width: 300px;
+`;
+
+const UserAccountBox = styled(S.AccountBox)`
+  justify-content: center;
+`;
+
+const UserContainer = styled(S.Container)`
+  @media only screen and (max-width: 1287px) {
+    margin: 0 calc((${window.innerWidth}px - 600px) / 2) !important;
+  }
+`;
+
+const UserSection = styled(S.Section)`
+  @media only screen and (max-width: 973px) {
+    display: block;
+  }
+`;
+
+const MainImg = styled.img.attrs({
+  src: 'images/user-img.png',
+})`
+  width: 620px;
+  height: 400px;
+  margin: 10px;
 `;
 
 export default User;
